b42c830a2c2955e3b5f3d0cdc9d47231a18f4199
YevheniiaZhu <evgeniya.zhukovskaya@gmail.com>
2021-07-20 11:57:36 +0300

    fixes syntax

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import {BrowserRouter as Router} from 'react-router-dom';
 import renderRoutes from './routes';
+import './App.css';
+
 
 function App() {
-  return (
-    <Router>
-      <div className="body">{renderRoutes()}</div>
-    </Router>
-  );
+    return (
+        <Router>
+            <div className='body'>{renderRoutes()}</div>
+        </Router>
+    );
 }
 
 export default App;
