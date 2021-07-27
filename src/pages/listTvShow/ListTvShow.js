9ece448ababc1452c5ccad45da70669a2e84698b
YevheniiaZhu <evgeniya.zhukovskaya@gmail.com>
2021-07-27 13:17:57 +0300

    M-11 Displayed lists of TV Shows and Movies on the pages with styled cards

--- src/pages/listTvShow/ListTvShow.js
+++ src/pages/listTvShow/ListTvShow.js
@@ -2,19 +2,6 @@ import React from 'react';
 import Card from '../../components/card/Card';
 import './listTvShow.css';
 
-const listTvShowCard = [
-    {
-        title: "List Tv Show Title",
-        image: "https://media.istockphoto.com/vectors/cinema-and-movie-time-vector-id640312764",
-        description: "List Tv Show Description"
-    },
-    {
-        title: "List Tv Show Title1",
-        image: "https://d2gg9evh47fn9z.cloudfront.net/800px_COLOURBOX24211921.jpg",
-        description: "List Tv Show Description1"
-    },
-]
-
 class ListTvShow extends React.Component {
 
     componentDidMount() {
